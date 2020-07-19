8fde4caf1da93eca648b96df9066dacfb5583254
Elaine Do <49426344+elainedo@users.noreply.github.com>
2020-07-18 23:11:08 -0500

    Add StaminaStats that includes stamina change rate for all unit states

--- src/model/units/unit_stats/UnitStats.java
+++ src/model/units/unit_stats/UnitStats.java
@@ -10,9 +10,8 @@ public class UnitStats {
     public double speed;
     public double rotationSpeed;
     public int patience;
-    public double maxStamina; // default stamina at the start of the game
-    public double staminaDepletionRate;
-    public double staminaRecoveryRate;
+
+    public StaminaStats staminaStats = new StaminaStats();
 
     // Archer & skirmisher stats
     public double widthVariation;
